0ff4fa229709fe482b1d05804c047ce1d9535314
Arun Rathore <arathore@nowoptics.com>
2023-11-17 10:49:17 +0530

    Initial Commit

---
+++ Dummy/DummyApp.swift
@@ -0,0 +1,17 @@
+//
+//  DummyApp.swift
+//  Dummy
+//
+//  Created by Arun Rathore on 17/11/23.
+//
+
+import SwiftUI
+
+@main
+struct DummyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
